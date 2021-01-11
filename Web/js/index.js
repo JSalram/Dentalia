c1f220fa0e6774e74297fce651f489990bfccbea
Hamza Mohammad <hamzzza56@gmail.com>
2021-01-11 17:26:57 +0100

    Añade scroll up arrow en index

--- Web/js/index.js
+++ Web/js/index.js
@@ -1,3 +1,21 @@
+const icon = document.createElement("i");
+icon.classList.add("fas", "fa-arrow-up");
+
+const scrollTop = document.createElement("div");
+scrollTop.className = "btnScrollTop btnScrollTopHide";
+
+scrollTop.appendChild(icon);
+document.body.appendChild(scrollTop);
+
+const btnScrollTop = document.querySelector(".btnScrollTop");
+
+btnScrollTop.addEventListener("click", function ()
+{
+    window.scrollTo(0, 0);    
+})
+
+let viewPortHeight = window.innerHeight;
+
 let nav = document.querySelector("nav.navbar");
 
 window.onscroll = function () {
@@ -13,5 +31,13 @@ function scrollFunction() {
         nav.style.cssText = "\
         background: transparent;\
         padding-top: 20px 1em 0 1em";
+    }
+    if (window.scrollY <= viewPortHeight)
+    {
+        btnScrollTop.className = "btnScrollTop btnScrollTopHide";
+    }
+    else
+    {
+        btnScrollTop.className = "btnScrollTop btnScrollTopShow";
     }
 }
