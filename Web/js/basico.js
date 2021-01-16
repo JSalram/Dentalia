e678a301c2f4c3c9654f101261bd2d32f8c682b3
Hamza Mohammad <hamzzza56@gmail.com>
2021-01-16 19:38:30 +0100

    Añade smoothness a scroll top

--- Web/js/basico.js
+++ Web/js/basico.js
@@ -11,7 +11,12 @@ const btnScrollTop = document.querySelector(".btnScrollTop");
 
 btnScrollTop.addEventListener("click", function ()
 {
-    window.scrollTo(0, 0);    
+    window.scrollTo
+    ({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });    
 })
 
 let viewPortHeight = window.innerHeight;
