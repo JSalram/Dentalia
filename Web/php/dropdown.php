6cdce19b76848a158fcf9540684ad01d58452aa6
Javier Salado Ramos <57096112+JSalram@users.noreply.github.com>
2021-03-06 12:56:34 +0100

    Login y mejoras registro, index y noticias

--- Web/php/dropdown.php
+++ Web/php/dropdown.php
@@ -2,21 +2,17 @@
 
 session_start();
 // $_SESSION["user"] = "Javi";
-// $_SESSION["admin"] = 1;
 $path = $_GET['path'];
 
 if (isset($_POST["logout"])) {
     session_destroy();
+    unset($_COOKIE);
+    setcookie("user", "", time() - 3600);
     header("Location: ../index.html");
 }
 
-if (isset($_SESSION["user"])) {
-    if (isset($_SESSION["admin"])) {
-        echo '<li><a href="' . $path . '/php/admin.php" class="dropdown-item">Administrar</a></li>';
-    } else {
-        echo '<li><a href="' . $path . '/nav/carrito.html" class="dropdown-item">Ver carro</a></li>';
-    }
-
+if (isset($_SESSION["user"]) || isset($_COOKIE["user"])) {
+    echo '<li><a href="' . $path . '/nav/carrito.html" class="dropdown-item">Ver carro</a></li>';
     echo '
     <li>
         <form action="' . $path . '/php/dropdown.php" method="POST">
